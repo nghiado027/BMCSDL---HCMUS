e23ecde700c19141653aa4ada7e35419d76f0906
NghiaDo <44922671+nghiado027@users.noreply.github.com>
2021-06-25 07:17:44 +0700

    Add files via upload

---
+++ Group/Lab04/source code/lab_nhom/DAO/DAO_NhanVien.cs
@@ -0,0 +1,59 @@
+﻿using lab_nhom.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lab_nhom.DAO
+{
+    public class DAO_NhanVien
+    {
+        private static DAO_NhanVien Instance = null;
+        private DAO_NhanVien()
+        {
+            
+        }
+        public static DAO_NhanVien get_Instance()
+        {
+            if(Instance == null)
+            {
+                Instance = new DAO_NhanVien();
+            }
+            return Instance;
+        }
+        public List<DTO_NhanVien> TaiDanhSachNhanVien()
+        {
+            List<DTO_NhanVien> ketQua = new List<DTO_NhanVien>();
+            DataProvider dp = new DataProvider();
+            String query = "EXEC SP_SEL_PUBLIC_ENCRYPT_NHANVIEN";
+            DataTable dt = dp.ExecuteQuery(query);
+            foreach (DataRow dtr in dt.Rows)
+            {
+                DTO_NhanVien NV = new DTO_NhanVien(dtr);
+                ketQua.Add(NV);
+            }
+            return ketQua;
+        }
+        public void ThemNhanVien(DTO_NhanVien nv)
+        {
+            DataProvider dp = new DataProvider();
+            object[] data = new object[7] { nv.maNV, nv.hotenNV, nv.emailNV, nv.Luong, nv.tendnNV, nv.matkhauNV, nv.pubkey };
+            dp.ExecuteNonQuery("EXEC SP_INS_PUBLIC_ENCRYPT_NHANVIEN  @MANV , @HOTEN , @EMAIL , @LUONG , @TENDN , @MATKHAU , @PUB", data);
+        }
+        public void XoaNhanVien(DTO_NhanVien nv)
+        {
+            DataProvider dp = new DataProvider();
+            String query = "EXEC SP_DEL_NHANVIEN '" + nv.maNV + "' ";
+            dp.ExecuteNonQuery(query);
+        }
+        public void SuaNhanVien(DTO_NhanVien nv)
+        {
+            DataProvider dp = new DataProvider();
+            object[] data = new object[7] { nv.maNV, nv.hotenNV, nv.emailNV, nv.Luong, nv.tendnNV, nv.matkhauNV, nv.pubkey };
+            dp.ExecuteNonQuery("EXEC SP_UPD_NHANVIEN  @MANV , @HOTEN , @EMAIL , @LUONG , @TENDN , @MATKHAU , @PUB", data);
+        }
+    }
+}
---
+++ Group/Lab04/source code/lab_nhom/DTO/DTO_SinhVien.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lab_nhom.DTO
+{
+    public class DTO_SinhVien
+    {
+        public String maSV;
+        public String tenSV;
+        public DateTime ngaysinhSV;
+        public String diachiSV;
+        public String malopSV;
+        public String tendnSV;
+        public byte[] matkhauSV;
+
+        public DTO_SinhVien()
+        { }
+        public DTO_SinhVien(DataRow dtr)
+        {
+            maSV = (String)dtr["MASV"];
+            tenSV = (String)dtr["HOTEN"];
+            ngaysinhSV = (DateTime)dtr["NGAYSINH"];
+            diachiSV = (String)dtr["DIACHI"];
+            malopSV = (String)dtr["MALOP"];
+            tendnSV = (String)dtr["TENDN"];
+            matkhauSV = (byte[])dtr["MATKHAU"];
+        }
+    }
+}
